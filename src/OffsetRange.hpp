8a747932aef127901f04de1b85665bd86681ffbd
Rafal Przywarski <>
2013-09-01 16:00:36 +0200

    OffsetRangeTest two_degenerate_ranges_do_not_overlap

--- src/OffsetRange.hpp
+++ src/OffsetRange.hpp
@@ -4,8 +4,11 @@
 class OffsetRange
 {
 public:
+    OffsetRange() { }
+    OffsetRange(unsigned, unsigned) { }
     bool empty() const { return true; }
     unsigned length() const { return 0; }
+    bool overlapsWith(const OffsetRange& ) { }
 };
 
 #endif // OFFSETRANGE_HPP
--- test/OffsetRangeTest.cpp
+++ test/OffsetRangeTest.cpp
@@ -1,12 +1,27 @@
 #include <gtest/gtest.h>
 #include <OffsetRange.hpp>
 
-TEST(OffsetRangeTest, should_be_empty_by_default)
+struct OffsetRangeTest : testing::Test
+{
+    void expectNoOverlapping(OffsetRange left, OffsetRange right, const std::string& desc)
+    {
+        ASSERT_FALSE(left.overlapsWith(right)) << desc;
+        ASSERT_FALSE(right.overlapsWith(left)) << desc;
+    }
+};
+
+TEST_F(OffsetRangeTest, should_be_empty_by_default)
 {
     ASSERT_TRUE(OffsetRange().empty());
 }
 
-TEST(OffsetRangeTest, should_have_zero_length_by_default)
+TEST_F(OffsetRangeTest, should_have_zero_length_by_default)
 {
     ASSERT_EQ(0, OffsetRange().length());
+}
+
+TEST_F(OffsetRangeTest, two_degenerate_ranges_do_not_overlap)
+{
+    expectNoOverlapping({2, 2}, {2, 2}, "same ranges");
+    expectNoOverlapping({1, 1}, {2, 2}, "different ranges");
 }
