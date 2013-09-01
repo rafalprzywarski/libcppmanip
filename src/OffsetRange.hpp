9506f53114b6ef28998758c7af018961d0aeda7d
Rafal Przywarski <>
2013-09-01 16:56:21 +0200

    OffsetRange should_detect_degenerate_ranges, denerate_ranges_never_overlap, disjoint_ranges_do_not_overlap, should_detect_overlapping_ranges

--- src/OffsetRange.hpp
+++ src/OffsetRange.hpp
@@ -7,11 +7,18 @@
 class OffsetRange
 {
 public:
-    OffsetRange() { }
-    OffsetRange(unsigned, unsigned) { }
-    bool degenerate() const { return true; }
+    OffsetRange() : from(0), to(0) { }
+    OffsetRange(unsigned from, unsigned to) : from(from), to(to) { }
+    bool degenerate() const { return from == to; }
     unsigned length() const { return 0; }
-    bool overlapsWith(const OffsetRange& ) { }
+    bool overlapsWith(const OffsetRange& right) const
+    {
+        if (right.from < from)
+            return right.overlapsWith(*this);
+        return !right.degenerate() && (right.from < to);
+    }
+private:
+    unsigned from, to;
 };
 
 #endif // OFFSETRANGE_HPP
--- test/OffsetRangeTest.cpp
+++ test/OffsetRangeTest.cpp
@@ -8,6 +8,11 @@ struct OffsetRangeTest : testing::Test
         ASSERT_FALSE(left.overlapsWith(right)) << desc;
         ASSERT_FALSE(right.overlapsWith(left)) << desc;
     }
+    void expectOverlapping(OffsetRange left, OffsetRange right, const std::string& desc)
+    {
+        ASSERT_TRUE(left.overlapsWith(right)) << desc;
+        ASSERT_TRUE(right.overlapsWith(left)) << desc;
+    }
 };
 
 TEST_F(OffsetRangeTest, should_be_degenerate_by_default)
@@ -20,8 +25,35 @@ TEST_F(OffsetRangeTest, should_have_zero_length_by_default)
     ASSERT_EQ(0, OffsetRange().length());
 }
 
+TEST_F(OffsetRangeTest, should_detect_degenerate_ranges)
+{
+    ASSERT_TRUE(OffsetRange(2, 2).degenerate());
+    ASSERT_FALSE(OffsetRange(3, 5).degenerate());
+}
+
 TEST_F(OffsetRangeTest, two_degenerate_ranges_do_not_overlap)
 {
     expectNoOverlapping({2, 2}, {2, 2}, "same ranges");
     expectNoOverlapping({1, 1}, {2, 2}, "different ranges");
+}
+
+TEST_F(OffsetRangeTest, denerate_ranges_never_overlap)
+{
+    expectNoOverlapping({1, 3}, {1, 1}, "beginnig");
+    expectNoOverlapping({1, 3}, {2, 2}, "middle");
+    expectNoOverlapping({1, 3}, {3, 3}, "ending");
+}
+
+TEST_F(OffsetRangeTest, disjoint_ranges_do_not_overlap)
+{
+    expectNoOverlapping({1, 3}, {3, 5}, "near");
+    expectNoOverlapping({1, 3}, {4, 6}, "far");
+}
+
+TEST_F(OffsetRangeTest, should_detect_overlapping_ranges)
+{
+    expectOverlapping({1, 4}, {3, 5}, "intersecting");
+    expectOverlapping({1, 10}, {3, 7}, "containing");
+    expectOverlapping({1, 4}, {1, 3}, "common beginning");
+    expectOverlapping({1, 5}, {2, 5}, "common ending");
 }
