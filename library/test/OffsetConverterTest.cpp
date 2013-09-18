0c778a751f921f4605453b3c87cc2be8e340ea2c
Rafal Przywarski <>
2013-09-18 21:13:39 +0200

    OffsetConverterTest made stricter

--- library/test/OffsetConverterTest.cpp
+++ library/test/OffsetConverterTest.cpp
@@ -12,13 +12,13 @@ TEST(OffsetConverterTest, should_return_offset_in_the_first_line)
 
 TEST(OffsetConverterTest, should_return_offset_from_the_last_eol_as_col)
 {
-    OffsetConverter converter("x\nx\nxxxxx");
+    OffsetConverter converter("x\nx\nxxxxx\n");
     ASSERT_EQ(2u, converter.getLocationFromOffset(6).col);
 }
 
 TEST(OffsetConverterTest, should_return_number_of_eols_till_offset_as_row)
 {
-    OffsetConverter converter("x\n\n\nxxxxx");
+    OffsetConverter converter("x\n\n\nxxxxx\n");
     EXPECT_EQ(0u, converter.getLocationFromOffset(0).row);
     EXPECT_EQ(3u, converter.getLocationFromOffset(4).row);
     EXPECT_EQ(3u, converter.getLocationFromOffset(5).row);
