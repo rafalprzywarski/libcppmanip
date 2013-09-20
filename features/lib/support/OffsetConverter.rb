4379d04f1ac9b5d4fdd8c716bc92ccf3567ed06f
Rafal Przywarski <>
2013-09-20 22:23:49 +0200

    support OffsetConverter should return offset from the last eol as col

--- features/lib/support/OffsetConverter.rb
+++ features/lib/support/OffsetConverter.rb
@@ -5,6 +5,11 @@ class OffsetConverter
     @text = text
   end
   def getLocationFromOffset offset
-    SourceLocation.row_col 0, offset
+    SourceLocation.row_col 0, calc_column(offset)
+  end
+  private
+  def calc_column offset
+    last_eol = @text.rindex("\n", offset - 1) if offset > 0
+    last_eol ? offset - last_eol - 1 : offset
   end
 end
--- features/spec/OffsetCoverter_spec.rb
+++ features/spec/OffsetCoverter_spec.rb
@@ -9,4 +9,10 @@ describe :OffsetCoverter do
     loc.row.should eq(0)
     conv.getLocationFromOffset(0).col.should eq(0)
   end
+  it "should return offset from the last eol as col" do
+    conv = OffsetConverter.new("x\nx\nxxxxx\n");
+    conv.getLocationFromOffset(3).col.should eq(1)
+    conv.getLocationFromOffset(4).col.should eq(0)
+    conv.getLocationFromOffset(5).col.should eq(1)
+  end
 end
