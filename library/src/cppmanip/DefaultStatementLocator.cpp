7a2f4ec2264e2a4fe0a0b319be19ef8879a378a3
Rafal Przywarski <>
2013-09-23 21:15:22 +0200

    DefaultStatementLocator should_return_an_empty_range_when_no_statement_touches_the_selection

--- library/src/cppmanip/DefaultStatementLocator.cpp
+++ library/src/cppmanip/DefaultStatementLocator.cpp
@@ -7,13 +7,12 @@ namespace cppmanip
 
 clang::StmtRange DefaultStatementLocator::findStatementsInFunction(const clang::FunctionDecl& decl)
 {
-    auto first = decl.getBody()->child_begin();
-    if (first == decl.getBody()->child_end())
-        return clang::StmtRange();
-    while (!getStmtRange(decl.getASTContext().getSourceManager(), **first).overlapsWith(selection))
-        ++first;
-    auto last = first;
-    ++last;
+    auto first = std::find_if(
+        decl.getBody()->child_begin(), decl.getBody()->child_end(),
+        [&](clang::Stmt *s) { return getStmtRange(decl.getASTContext().getSourceManager(), *s).overlapsWith(selection); });
+    auto last = std::find_if(
+        first, decl.getBody()->child_end(),
+        [&](clang::Stmt *s) { return !getStmtRange(decl.getASTContext().getSourceManager(), *s).overlapsWith(selection); });
     return clang::StmtRange(first, last);
 }
 
--- library/test/cppmanip/DefaultStatementLocatorTest.cpp
+++ library/test/cppmanip/DefaultStatementLocatorTest.cpp
@@ -21,7 +21,7 @@ struct DefaultStatementLocatorTest : testing::Test
     {
         function.reset(new test::ParsedFunction(source));
         locator.reset(new DefaultStatementLocator(
-            std::bind(&DefaultStatementLocatorTest::getStmtRange, this, std::placeholders::_1, std::placeholders::_2),
+            [&](clang::SourceManager& sm, clang::Stmt& s) { return getStmtRange(sm, s); },
             selection));
     }
 
@@ -46,15 +46,26 @@ TEST_F(DefaultStatementLocatorTest, should_return_an_empty_range_for_an_empty_fu
     ASSERT_TRUE(stmts.empty());
 }
 
-TEST_F(DefaultStatementLocatorTest, should_return_the_statement_touching_the_selection)
+TEST_F(DefaultStatementLocatorTest, should_return_the_statements_touching_the_selection)
 {
-    parseSourceAndCreateLocatorForSelection("void f() {\n  int x;\n  int y;\n  int z;\n}\n", { rowCol(2, 0), rowCol(2, 4) });
+    parseSourceAndCreateLocatorForSelection("void f() {\n  int x;\n  int y;\n  int z;\n  int w;\n}\n", { rowCol(2, 0), rowCol(3, 4) });
     expectGetRangeForStmtAndReturn(0, { rowCol(1, 2), rowCol(1, 8) });
     expectGetRangeForStmtAndReturn(1, { rowCol(2, 2), rowCol(2, 8) });
     expectGetRangeForStmtAndReturn(2, { rowCol(3, 2), rowCol(3, 8) });
+    expectGetRangeForStmtAndReturn(3, { rowCol(4, 2), rowCol(4, 8) });
     auto stmts = locator->findStatementsInFunction(*function->getDecl());
-    ASSERT_EQ(1u, std::distance(begin(stmts), end(stmts)));
+    ASSERT_EQ(2u, std::distance(begin(stmts), end(stmts)));
     ASSERT_TRUE(*stmts == nthStmt(1));
+    ASSERT_TRUE(*boost::next(stmts) == nthStmt(2));
+}
+
+TEST_F(DefaultStatementLocatorTest, should_return_an_empty_range_when_no_statement_touches_the_selection)
+{
+    parseSourceAndCreateLocatorForSelection("void f() {\n  int a;\nint b;}", { rowCol(0, 1), rowCol(0, 3) });
+    expectGetRangeForStmtAndReturn(0, { rowCol(1, 2), rowCol(1, 8) });
+    expectGetRangeForStmtAndReturn(1, { rowCol(2, 0), rowCol(2, 6) });
+    auto stmts = locator->findStatementsInFunction(*function->getDecl());
+    ASSERT_TRUE(stmts.empty());
 }
 
 }
