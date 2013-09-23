7d0a763eb9c10766831d0d37692880950e9f1363
Rafal Przywarski <>
2013-09-23 18:29:43 +0200

    refactored ClangFunctionExtractor

--- library/src/cppmanip/ClangFunctionExtractor.cpp
+++ library/src/cppmanip/ClangFunctionExtractor.cpp
@@ -7,21 +7,22 @@
 
 namespace cppmanip
 {
-
-SourceReplacements recordReplacements(const text::OffsetBasedTextModifier& sourceOperations, const std::string& source)
+namespace
 {
-    OffsetConverter offsetCoverter(source);
+SourceReplacements recordReplacements(const text::OffsetBasedTextModifier& sourceOperations, const std::string& filename)
+{
+    OffsetConverter offsetCoverter(io::loadTextFromFile(filename));
     text::TextReplacementRecorder recorder(std::bind(&OffsetConverter::getLocationFromOffset, &offsetCoverter, std::placeholders::_1));
     sourceOperations.apply(recorder);
     return recorder.getReplacements();
 }
+}
 
 SourceReplacements ClangFunctionExtractor::extractFunctionInFile(const std::string& functionName, SourceSelection selection, const std::string& filename)
 {
-    std::string source = io::loadTextFromFile(filename);
     auto functionExtractor = TranslationUnitFunctionExtractorFactory().createFunctionExtractor(functionName, LocationRange(selection.from, selection.to), textModifier);
     clangutil::runTranslationUnitHandlerOnFile(functionExtractor, filename);
-    return recordReplacements(textModifier, source);
+    return recordReplacements(textModifier, filename);
 }
 
 }
