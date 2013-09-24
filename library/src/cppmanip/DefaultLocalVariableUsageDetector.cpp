5926e2ecfad69e71af99f1ca8b0d0f90fb5defe9
Rafal Przywarski <>
2013-09-24 23:38:50 +0200

    DefaultLocalVariableUsageDetector should_return_variables_in_given_range

--- library/src/cppmanip/DefaultLocalVariableUsageDetector.cpp
+++ library/src/cppmanip/DefaultLocalVariableUsageDetector.cpp
@@ -1,12 +1,34 @@
 #include "DefaultLocalVariableUsageDetector.hpp"
+#include <clang/AST/RecursiveASTVisitor.h>
 
 namespace cppmanip
 {
 
+namespace
+{
+
+struct UsedVariablesVisitor : clang::RecursiveASTVisitor<UsedVariablesVisitor>
+{
+    LocalVariableUsageDetector::Variables used;
+
+    bool VisitDeclRefExpr(clang::DeclRefExpr *d)
+    {
+        auto var = clang::dyn_cast<clang::VarDecl>(d->getDecl());
+        if (var)
+            used.push_back(var);
+        return true;
+    }
+};
+
+}
+
 LocalVariableUsageDetector::Variables DefaultLocalVariableUsageDetector::findLocalVariablesRequiredForStmts(
     clang::StmtRange stmts)
 {
-    return Variables();
+    UsedVariablesVisitor v;
+    for (auto s : stmts)
+        v.TraverseStmt(s);
+    return v.used;
 }
 LocalVariableUsageDetector::Variables DefaultLocalVariableUsageDetector::findVariablesDeclaredByAndUsedAfterStmts(
     clang::StmtRange stmts, clang::Stmt& parent)
--- library/test/cppmanip/DefaultLocalVariableUsageDetectorTest.cpp
+++ library/test/cppmanip/DefaultLocalVariableUsageDetectorTest.cpp
@@ -2,6 +2,8 @@
 #include "ParsedFunction.hpp"
 #include <gtest/gtest.h>
 #include <memory>
+#include <boost/algorithm/string/join.hpp>
+#include <clang/AST/Stmt.h>
 
 namespace cppmanip
 {
@@ -18,8 +20,27 @@ struct DefaultLocalVariableUsageDetectorTest : testing::Test
 
     clang::StmtRange parseStmts(const std::string& stmts)
     {
-        func.reset(new test::ParsedFunction(extraDeclarations + "void func__() {" + stmts + "}"));
+        auto source = extraDeclarations + "void func__() {" + stmts + "}";
+        func.reset(new test::ParsedFunction(source));
         return func->stmts();
+    }
+
+    clang::StmtRange skip(unsigned n, clang::StmtRange r)
+    {
+        return { boost::next(begin(r), n), end(r) };
+    }
+
+    clang::VarDecl *varDecl(unsigned n, clang::StmtRange stmts)
+    {
+        return clang::dyn_cast<clang::VarDecl>(clang::dyn_cast<clang::DeclStmt>(*boost::next(begin(stmts), n))->getSingleDecl());
+    }
+
+    void expectEqUnordered(LocalVariableUsageDetector::Variables found, LocalVariableUsageDetector::Variables expected)
+    {
+        ASSERT_EQ(expected.size(), found.size());
+        std::sort(found.begin(), found.end());
+        std::sort(expected.begin(), expected.end());
+        ASSERT_TRUE(expected == found);
     }
 };
 
@@ -30,6 +51,18 @@ TEST_F(DefaultLocalVariableUsageDetectorTest, should_return_no_variables_of_none
     auto stmts = parseStmts("f(); g();");
 
     ASSERT_TRUE(detector.findLocalVariablesRequiredForStmts(stmts).empty());
+}
+
+TEST_F(DefaultLocalVariableUsageDetectorTest, should_return_variables_in_given_range)
+{
+    DefaultLocalVariableUsageDetector detector;
+    declareFunctions("void f(int); void g(int);");
+    auto stmts = parseStmts("int x = 1; int y = 2; f(x); g(y);");
+    const auto INT_X = 0, INT_Y = 1;
+    auto checked = skip(2, stmts);
+
+    auto found = detector.findLocalVariablesRequiredForStmts(checked);
+    expectEqUnordered(found, { varDecl(INT_X, stmts), varDecl(INT_Y, stmts) });
 }
 
 }
