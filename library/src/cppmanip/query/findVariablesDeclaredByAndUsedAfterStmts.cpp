4fa53e707d21b81792e9a6dc6eb3354d342e8e68
Rafal Przywarski <>
2013-09-29 14:39:33 +0200

    findVariablesDeclaredByAndUsedAfterStmts should_return_variables_used_after_given_statements

--- library/src/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmts.cpp
+++ library/src/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmts.cpp
@@ -1,13 +1,38 @@
 #include "findVariablesDeclaredByAndUsedAfterStmts.hpp"
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <unordered_set>
 
 namespace cppmanip
 {
 namespace query
 {
+namespace
+{
+
+class UsedVariablesVisitor : public clang::RecursiveASTVisitor<UsedVariablesVisitor>
+{
+public:
+    bool VisitDeclRefExpr(clang::DeclRefExpr *d)
+    {
+        auto var = clang::dyn_cast<clang::VarDecl>(d->getDecl());
+        if (var)
+            used.insert(var);
+        return true;
+    }
+
+    std::vector<clang::VarDecl *> getUsed() const { return { begin(used), end(used) }; }
+private:
+    std::unordered_set<clang::VarDecl *> used;
+};
+
+}
 
 std::vector<clang::VarDecl *> findVariablesDeclaredByAndUsedAfterStmts(clang::StmtRange stmts, clang::Stmt& parent)
 {
-    return std::vector<clang::VarDecl *>();
+    UsedVariablesVisitor v;
+    for (auto stmt : clang::StmtRange(end(stmts), parent.child_end()))
+        v.TraverseStmt(stmt);
+    return v.getUsed();
 }
 
 }
--- library/test/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmtsTest.cpp
+++ library/test/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmtsTest.cpp
@@ -20,21 +20,26 @@ struct findVariablesDeclaredByAndUsedAfterStmtsTest : testing::Test
         extraDeclarations = functions;
     }
 
-    clang::StmtRange parseStmts(const std::string& stmts)
+    struct StmtsWithScope
+    {
+        clang::StmtRange range;
+        clang::Stmt& scope;
+    };
+    StmtsWithScope parseStmts(const std::string& stmts)
     {
         auto source = extraDeclarations + "void func__() {" + stmts + "}";
         func.reset(new test::ParsedFunction(source));
-        return func->stmts();
+        return { func->stmts(), *func->getDecl()->getBody() };
     }
 
-    clang::StmtRange skip(unsigned n, clang::StmtRange r)
+    clang::StmtRange first(unsigned n, clang::StmtRange r)
     {
-        return { boost::next(begin(r), n), end(r) };
+        return { begin(r), boost::next(begin(r), n) };
     }
 
-    clang::VarDecl *varDecl(unsigned n, clang::StmtRange stmts)
+    clang::VarDecl *varDecl(unsigned n, StmtsWithScope stmts)
     {
-        return clang::dyn_cast<clang::VarDecl>(clang::dyn_cast<clang::DeclStmt>(*boost::next(begin(stmts), n))->getSingleDecl());
+        return clang::dyn_cast<clang::VarDecl>(clang::dyn_cast<clang::DeclStmt>(*boost::next(begin(stmts.range), n))->getSingleDecl());
     }
 
     void expectEqUnordered(std::vector<clang::VarDecl *> found, std::vector<clang::VarDecl *> expected)
@@ -44,20 +49,24 @@ struct findVariablesDeclaredByAndUsedAfterStmtsTest : testing::Test
         std::sort(expected.begin(), expected.end());
         ASSERT_TRUE(expected == found);
     }
-
-    void expectEqOrdered(std::vector<clang::VarDecl *> found, std::vector<clang::VarDecl *> expected)
-    {
-        ASSERT_EQ(expected.size(), found.size());
-        ASSERT_TRUE(expected == found);
-    }
 };
 
-TEST_F(findVariablesDeclaredByAndUsedAfterStmtsTest, should_return_no_variables_of_none_are_declared)
+TEST_F(findVariablesDeclaredByAndUsedAfterStmtsTest, should_return_no_variables_if_none_are_declared)
 {
     declareGlobal("void f(); void g();");
     auto stmts = parseStmts("f(); g();");
 
-    ASSERT_TRUE(findVariablesDeclaredByAndUsedAfterStmts(stmts, *func->getDecl()->getBody()).empty());
+    ASSERT_TRUE(findVariablesDeclaredByAndUsedAfterStmts(stmts.range, stmts.scope).empty());
+}
+
+TEST_F(findVariablesDeclaredByAndUsedAfterStmtsTest, should_return_variables_used_after_given_statements)
+{
+    declareGlobal("void f(int); void g(int);");
+    auto stmts = parseStmts("int i = 1; int j = 2; int x = 3; f(j); g(i + j);");
+    const auto INT_I = 0, INT_J = 1;
+
+    auto found = findVariablesDeclaredByAndUsedAfterStmts(first(3, stmts.range), stmts.scope);
+    expectEqUnordered(found, { varDecl(INT_I, stmts), varDecl(INT_J, stmts) });
 }
 
 }
