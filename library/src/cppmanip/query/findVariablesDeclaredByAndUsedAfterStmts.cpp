f3c1d7fe87f207bf02c2c3ae061025b6707cadf4
Rafal Przywarski <>
2013-09-29 15:08:52 +0200

    findVariablesDeclaredByAndUsedAfterStmts should_only_return_variables_declared_by_given_statements

--- library/src/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmts.cpp
+++ library/src/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmts.cpp
@@ -1,6 +1,8 @@
 #include "findVariablesDeclaredByAndUsedAfterStmts.hpp"
 #include <clang/AST/RecursiveASTVisitor.h>
 #include <unordered_set>
+#include <boost/range/adaptor/filtered.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
 
 namespace cppmanip
 {
@@ -25,14 +27,45 @@ private:
     std::unordered_set<clang::VarDecl *> used;
 };
 
+class DeclaredVariablesVisitor : public clang::RecursiveASTVisitor<DeclaredVariablesVisitor>
+{
+public:
+    bool VisitVarDecl(clang::VarDecl *d)
+    {
+        declared.insert(d);
+        return true;
+    }
+    std::unordered_set<clang::VarDecl *> getDeclared() const { return declared; }
+private:
+    std::unordered_set<clang::VarDecl *> declared;
+};
+
+std::vector<clang::VarDecl *> findVariablesUsedByStmts(clang::StmtRange stmts)
+{
+    UsedVariablesVisitor v;
+    for (auto stmt : stmts)
+        v.TraverseStmt(stmt);
+    return v.getUsed();
+}
+
+std::unordered_set<clang::VarDecl *> findVariablesDeclaredByStmts(clang::StmtRange stmts)
+{
+    DeclaredVariablesVisitor v;
+    for (auto stmt : stmts)
+        v.TraverseStmt(stmt);
+    return v.getDeclared();
+}
+
 }
 
 std::vector<clang::VarDecl *> findVariablesDeclaredByAndUsedAfterStmts(clang::StmtRange stmts, clang::Stmt& parent)
 {
-    UsedVariablesVisitor v;
-    for (auto stmt : clang::StmtRange(end(stmts), parent.child_end()))
-        v.TraverseStmt(stmt);
-    return v.getUsed();
+    auto declared = findVariablesDeclaredByStmts(stmts);
+    auto used = findVariablesUsedByStmts({end(stmts), parent.child_end()});
+    std::vector<clang::VarDecl *> result;
+    auto isDeclared = [&](clang::VarDecl *d) { return declared.count(d) != 0; };
+    boost::push_back(result, used | boost::adaptors::filtered(isDeclared));
+    return result;
 }
 
 }
--- library/test/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmtsTest.cpp
+++ library/test/cppmanip/query/findVariablesDeclaredByAndUsedAfterStmtsTest.cpp
@@ -37,6 +37,11 @@ struct findVariablesDeclaredByAndUsedAfterStmtsTest : testing::Test
         return { begin(r), boost::next(begin(r), n) };
     }
 
+    clang::StmtRange skip(unsigned n, clang::StmtRange r)
+    {
+        return { boost::next(begin(r), n), end(r) };
+    }
+
     clang::VarDecl *varDecl(unsigned n, StmtsWithScope stmts)
     {
         return clang::dyn_cast<clang::VarDecl>(clang::dyn_cast<clang::DeclStmt>(*boost::next(begin(stmts.range), n))->getSingleDecl());
@@ -67,6 +72,17 @@ TEST_F(findVariablesDeclaredByAndUsedAfterStmtsTest, should_return_variables_use
 
     auto found = findVariablesDeclaredByAndUsedAfterStmts(first(3, stmts.range), stmts.scope);
     expectEqUnordered(found, { varDecl(INT_I, stmts), varDecl(INT_J, stmts) });
+}
+
+TEST_F(findVariablesDeclaredByAndUsedAfterStmtsTest, should_only_return_variables_declared_by_given_statements)
+{
+    declareGlobal("void f(int); void g(int);");
+    auto stmts = parseStmts("int x = 1; int y = 2; int z = 3; int w = 4; f(x + y); g(z + w);");
+    const auto INT_X = 0, INT_Y = 1, INT_Z = 2, INT_W = 3;
+
+    auto checked = first(2, skip(2, stmts.range));
+    auto found = findVariablesDeclaredByAndUsedAfterStmts(checked, stmts.scope);
+    expectEqUnordered(found, { varDecl(INT_Z, stmts), varDecl(INT_W, stmts) });
 }
 
 }
