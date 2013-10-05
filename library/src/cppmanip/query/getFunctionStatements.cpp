c9bfc315e94b274ed2a070760815847c51ff907e
Rafal Przywarski <>
2013-10-05 12:29:04 +0200

    getFunctionStatements should_return_no_declared_local_variables_when_a_statement_declares_none, should_return_local_variables_used_by_a_statement

--- library/src/cppmanip/query/getFunctionStatements.cpp
+++ library/src/cppmanip/query/getFunctionStatements.cpp
@@ -2,6 +2,12 @@
 #include <clang/AST/Decl.h>
 #include <clang/AST/Stmt.h>
 #include <clang/AST/ASTContext.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <boost/range/adaptor/transformed.hpp>
+#include <boost/range/algorithm_ext/push_back.hpp>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
 
 namespace cppmanip
 {
@@ -11,18 +17,73 @@ namespace query
 namespace
 {
 
-ast::LocalVariables getDeclaredVars(clang::Stmt& stmt)
+class UsedVariablesVisitor : public clang::RecursiveASTVisitor<UsedVariablesVisitor>
+{
+public:
+    bool VisitDeclRefExpr(clang::DeclRefExpr *d)
+    {
+        auto var = clang::dyn_cast<clang::VarDecl>(d->getDecl());
+        if (var)
+            used.insert(var);
+        return true;
+    }
+    std::vector<clang::VarDecl *> get() const { return {used.begin(), used.end()}; }
+private:
+    std::unordered_set<clang::VarDecl *> used;
+};
+
+typedef std::unordered_map<clang::VarDecl *, ast::LocalVariablePtr> LocalSymbols;
+
+std::vector<clang::VarDecl *> getUsedVarDecls(clang::Stmt& stmt)
+{
+    UsedVariablesVisitor v;
+    v.TraverseStmt(&stmt);
+    return v.get();
+}
+
+std::vector<clang::VarDecl *> getDeclaredVarDecls(clang::Stmt& stmt)
 {
     auto decl = clang::dyn_cast<clang::DeclStmt>(&stmt);
     if (!decl)
-        return ast::LocalVariables();
-    ast::LocalVariables vars;
+        return {};
+    std::vector<clang::VarDecl *> vars;
     for (auto d = decl->decl_begin(); d != decl->decl_end(); ++d)
-    {
-        auto v = clang::dyn_cast<clang::VarDecl>(*d);
-        vars.push_back(std::make_shared<ast::LocalVariable>(v->getNameAsString(), v->getType().getAsString() + " " + v->getNameAsString()));
-    }
+        vars.push_back(clang::dyn_cast<clang::VarDecl>(*d));
     return vars;
+}
+
+ast::LocalVariablePtr asLocalVariable(clang::VarDecl *v)
+{
+    return std::make_shared<ast::LocalVariable>(v->getNameAsString(), v->getType().getAsString() + " " + v->getNameAsString());
+}
+
+ast::LocalVariables getDeclaredVars(clang::Stmt& stmt, LocalSymbols& locals)
+{
+    ast::LocalVariables declared;
+    for (auto var : getDeclaredVarDecls(stmt))
+    {
+        auto local = asLocalVariable(var);
+        locals.insert({var, local});
+        declared.push_back(local);
+    }
+    return declared;
+}
+
+ast::LocalVariables getUsedLocalVars(clang::Stmt& stmt, const LocalSymbols& locals)
+{
+    ast::LocalVariables used;
+    for (auto var : getUsedVarDecls(stmt))
+        used.push_back(locals.find(var)->second);
+    return used;
+}
+
+template <typename GetStmtRange>
+ast::StatementPtr translateStmt(clang::Stmt& stmt, LocalSymbols& locals, GetStmtRange getStmtRange)
+{
+    auto declared = getDeclaredVars(stmt, locals);
+    auto used = getUsedLocalVars(stmt, locals);
+    auto range = getStmtRange(stmt);
+    return std::make_shared<ast::Statement>(range, declared, used);
 }
 
 }
@@ -30,9 +91,11 @@ ast::LocalVariables getDeclaredVars(clang::Stmt& stmt)
 ast::Statements getFunctionStatements(clang::FunctionDecl& f, GetStatementRange getStmtRange)
 {
     ast::Statements stmts;
+    LocalSymbols locals;
     for (auto stmt : f.getBody()->children())
-        stmts.push_back(std::make_shared<ast::Statement>(
-            getStmtRange(f.getASTContext().getSourceManager(), *stmt), getDeclaredVars(*stmt), ast::LocalVariables()));
+        stmts.push_back(
+            translateStmt(
+                *stmt, locals, [&](clang::Stmt& s) { return getStmtRange(f.getASTContext().getSourceManager(), s); }));
     return stmts;
 }
 
--- library/test/cppmanip/query/getFunctionStatementsTest.cpp
+++ library/test/cppmanip/query/getFunctionStatementsTest.cpp
@@ -3,6 +3,7 @@
 #include <clang/AST/Stmt.h>
 #include <gmock/gmock.h>
 #include "../gtestdef.hpp"
+#include <boost/next_prior.hpp>
 
 using namespace testing;
 
@@ -19,11 +20,17 @@ struct getFunctionStatementsTest : testing::Test
     getFunctionStatementsTest()
     {
         getStmtRange = [this](clang::SourceManager& sm, clang::Stmt& s) { return getStmtRangeMocked(sm, s); };
+        ALLOWING_FCALL(getStmtRangeMocked(_, _)).WillRepeatedly(Return(ast::SourceLocationRange(ast::rowCol(0, 0), ast::rowCol(0, 0))));
     }
 
     void parse(std::string source)
     {
         func.reset(new test::ParsedFunction(source));
+    }
+
+    clang::Stmt& stmtNo(int index)
+    {
+        return **boost::next(func->getDecl()->getBody()->child_begin(), index);
     }
 
     MOCK_METHOD2(getStmtRangeMocked, ast::SourceLocationRange(clang::SourceManager&, clang::Stmt& ));
@@ -33,8 +40,8 @@ TEST_F(getFunctionStatementsTest, should_return_the_ranges_of_each_statements)
 {
     parse("void f() {\n int x = 1; if (x == 1)\n; }");
     ast::SourceLocationRange LOC1{ { 1, 2 }, { 3, 4 } }, LOC2{ { 5, 6 }, { 7, 8 } };
-    EXPECT_FCALL(getStmtRangeMocked(_, Ref(**func->getDecl()->getBody()->child_begin()))).WillOnce(Return(LOC1));
-    EXPECT_FCALL(getStmtRangeMocked(_, Ref(**++func->getDecl()->getBody()->child_begin()))).WillOnce(Return(LOC2));
+    EXPECT_FCALL(getStmtRangeMocked(_, Ref(stmtNo(0)))).WillOnce(Return(LOC1));
+    EXPECT_FCALL(getStmtRangeMocked(_, Ref(stmtNo(1)))).WillOnce(Return(LOC2));
     auto stmts = getFunctionStatements(*func->getDecl(), getStmtRange);
 
     ASSERT_EQ(2u, stmts.size());
@@ -51,7 +58,6 @@ TEST_F(getFunctionStatementsTest, should_return_no_statements_when_the_given_fun
 TEST_F(getFunctionStatementsTest, should_return_local_variables_declared_by_a_statement)
 {
     parse("void f() {\n int x = 1, y = 2, z = 1; }");
-    ALLOWING_FCALL(getStmtRangeMocked(_, _)).WillRepeatedly(Return(ast::SourceLocationRange(ast::rowCol(0, 0), ast::rowCol(0, 0))));
     auto vars = getFunctionStatements(*func->getDecl(), getStmtRange)[0]->getDeclaredVariables();
     ASSERT_EQ(3u, vars.size());
     EXPECT_EQ("x", vars[0]->getName());
@@ -60,6 +66,20 @@ TEST_F(getFunctionStatementsTest, should_return_local_variables_declared_by_a_st
     EXPECT_EQ("int y", vars[1]->getNameWithType());
     EXPECT_EQ("z", vars[2]->getName());
     EXPECT_EQ("int z", vars[2]->getNameWithType());
+}
+
+TEST_F(getFunctionStatementsTest, should_return_no_declared_local_variables_when_a_statement_declares_none)
+{
+    parse("void f() {\n f(); }");
+    ASSERT_TRUE(getFunctionStatements(*func->getDecl(), getStmtRange)[0]->getDeclaredVariables().empty());
+}
+
+TEST_F(getFunctionStatementsTest, should_return_local_variables_used_by_a_statement)
+{
+    parse("void f(int) {\n int x = 2; int y = 3; int z = x; f(z + y); }");
+    auto stmts = getFunctionStatements(*func->getDecl(), getStmtRange);
+    ASSERT_TRUE(stmts[0]->getDeclaredVariables() == stmts[2]->getUsedLocalVariables());
+    ASSERT_EQ(2u, stmts[3]->getUsedLocalVariables().size());
 }
 
 }
