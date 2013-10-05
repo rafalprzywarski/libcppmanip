67256bb17f12430159169b0dd6bfb8cb0b3fedc7
Rafal Przywarski <>
2013-10-05 13:31:17 +0200

    simplified SourceLocationConverterTest

--- library/test/cppmanip/SourceLocationConverterTest.cpp
+++ library/test/cppmanip/SourceLocationConverterTest.cpp
@@ -8,30 +8,21 @@ namespace cppmanip
 TEST(SourceLocationConverterTest, should_return_column_at_row_0_as_offset)
 {
     SourceLocationConverter converter("xxxxxx");
-    boundary::SourceLocation loc;
-    loc.col = 3;
-    ASSERT_EQ(3u, converter.getOffsetFromLocation(loc));
+    ASSERT_EQ(3u, converter.getOffsetFromLocation(boundary::rowCol(0, 3)));
 }
 
 TEST(SourceLocationConverterTest, should_sum_offset_from_previous_lines)
 {
     SourceLocationConverter converter("xx\nxxx\nxx\nx");
-    boundary::SourceLocation loc;
-    loc.row = 2;
-    loc.col = 1;
-    ASSERT_EQ(8u, converter.getOffsetFromLocation(loc));
-    loc.row = 3;
-    ASSERT_EQ(11u, converter.getOffsetFromLocation(loc));
+    ASSERT_EQ(8u, converter.getOffsetFromLocation(boundary::rowCol(2, 1)));
+    ASSERT_EQ(11u, converter.getOffsetFromLocation(boundary::rowCol(3, 1)));
 }
 
 TEST(SourceLocationConverterTest, should_fail_of_row_is_greater_than_the_number_of_lines)
 {
     SourceLocationConverter converter("\n\n");
-    boundary::SourceLocation loc;
-    loc.row = 2;
-    ASSERT_NO_THROW(converter.getOffsetFromLocation(loc));
-    loc.row = 3;
-    ASSERT_THROW(converter.getOffsetFromLocation(loc), std::out_of_range);
+    ASSERT_NO_THROW(converter.getOffsetFromLocation(boundary::rowCol(2, 0)));
+    ASSERT_THROW(converter.getOffsetFromLocation(boundary::rowCol(3, 0)), std::out_of_range);
 }
 
 }
