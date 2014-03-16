7e22de9fb22c44f836399fd9d01cc41a51543fc5
Rafal Przywarski <>
2014-03-16 13:53:32 +0100

    fixed typos

--- library/test/cppmanip/query/isStatementSelectedTest.cpp
+++ library/test/cppmanip/query/isStatementSelectedTest.cpp
@@ -9,12 +9,12 @@ namespace query
 namespace test
 {
 
-TEST(isStatementSelectedTest, should_return_IMPLICITLY_when_a_statement_range_overlaps_with_a_selection)
+TEST(isStatementSelectedTest, should_return_IMPLICITLY_when_a_statement_range_overlaps_a_selection)
 {
     ASSERT_TRUE(isStatementSelected(stmtWithRange({1, 3}), { 0, 2 }) == StatementSelected::IMPLICITLY);
 }
 
-TEST(isStatementSelectedTest, should_return_NO_when_a_statement_range_does_not_overlap_with_a_selection)
+TEST(isStatementSelectedTest, should_return_NO_when_a_statement_range_does_not_overlap_a_selection)
 {
     ASSERT_TRUE(isStatementSelected(stmtWithRange({0, 1}), { 3, 4 }) == StatementSelected::NO);
 }
